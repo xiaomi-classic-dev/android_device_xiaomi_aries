30526064478f6939698d7712a5137e608d4f9b37
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-10 20:59:38 +0800

    aries: Remove mokee stuff
    Change-Id: Ic27e63796d233a0d12c2b5b8778959571d72cd04

--- mk_aries.mk
+++ mk_aries.mk
@@ -22,9 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/aries/aries.mk)
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_mini_phone.mk)
-
 PRODUCT_NAME := mk_aries
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_DEVICE := aries
