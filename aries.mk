bf2af46e82328156dc104152fb9574519e8bb3b7
GuaiYiHu <guaiyihu@foxmail.com>
2026-06-12 01:14:02 +0800

    aries: Set shipping API level

--- aries.mk
+++ aries.mk
@@ -3,6 +3,9 @@ DEVICE_PATH := device/xiaomi/aries
 $(call inherit-product, device/xiaomi/msm8960-common/msm8960-common.mk)
 $(call inherit-product, vendor/xiaomi/aries/aries-vendor.mk)
 
+# Mi 2 launched on Android 4.1 (API 16).
+PRODUCT_SHIPPING_API_LEVEL := 16
+
 # System properties
 -include $(DEVICE_PATH)/system_prop.mk
 
